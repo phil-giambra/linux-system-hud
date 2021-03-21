f3c499fdcee26a630c9bbac821d8912ff525fd77
Phil Giambra <philgiambra@gmail.com>
2021-03-21 07:25:03 -0400

    testing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const {app, BrowserWindow, ipcMain, Menu, globalShortcut} = require('electron')
-const { fork } = require('child_process');
+const { fork , spawn } = require('child_process');
 const fs = require('fs')
 const path = require('path')
 let args = process.argv
@@ -114,6 +114,17 @@ app.whenReady().then(() => {
 
     })
 
+    const ret2 = globalShortcut.register('Super+Tab', () => {
+        console.log('super Tab is pressed')
+        if (hudClock.isVisible()) {
+            hudClock.hide()
+        }
+        wincmd.raiseWindow(wincmd.list[wincmd.pos].id)
+        wincmd.pos += 1
+        if (wincmd.pos > wincmd.list.length - 1){ wincmd.pos = 0 }
+
+    })
+
     createHudClock()
 
 })
@@ -165,7 +176,9 @@ function createHudClock () {
         y:0,
         width: 1024,
         height: 768,
-        transparent:true,
+        //transparent:true,
+        frame:false,
+        //resizable:false,
         webPreferences: {
             contextIsolation: false,
             preload: path.join(__dirname, 'preload.js')
@@ -176,7 +189,7 @@ function createHudClock () {
     // and load the index.html of the app.
     hudClock.loadFile( path.join(__dirname,'huds/hud_clock.html') )
     // Open the DevTools.
-    hudClock.webContents.openDevTools()
+    //hudClock.webContents.openDevTools()
 }
 
 
@@ -201,3 +214,98 @@ ipcMain.on("hud_clock_window", (event, data) => {
     }
 
 })
+
+let wincmd = {
+
+
+}
+wincmd.list = []
+wincmd.pos = 0
+wincmd.raiseWindow = function(id) {
+    let databuf = ""
+    let raise = spawn("xdo", ["raise", id ])
+       raise.stdout.on('data', (data) => { databuf += data });
+       raise.stderr.on('data', (data) => { console.log("stderr",data.toString());});
+       raise.on('exit', (code) => {
+         console.log(`raise exited with code ${code}`);
+
+
+     })
+     let activate = spawn("xdo", ["activate", id ])
+        activate.stdout.on('data', (data) => { databuf += data });
+        activate.stderr.on('data', (data) => { console.log("stderr",data.toString());});
+        activate.on('exit', (code) => {
+          console.log(`activate exited with code ${code}`);
+          console.log("wincmd.raiseWindow",id, databuf);
+
+      })
+}
+
+
+wincmd.getScreenSize = function(){
+    let databuf = ""
+    let probespawn = spawn("sh", ["-c","xrandr --current | grep '*+'" ])
+       probespawn.stdout.on('data', (data) => { databuf += data });
+       probespawn.stderr.on('data', (data) => { console.log("stderr",data.toString());});
+       probespawn.on('exit', (code) => {
+         //console.log(`probespawn exited with code ${code}`);
+         //
+         let s = databuf.trim().split("\n")
+         s.forEach((item, i) => {
+             s[i] = item.trim().split(" ")
+             s[i] = s[i][0].trim().split("x")
+         });
+         console.log("wincmd.getScreenSize",s[0]);
+         //return s[0]
+     })
+}
+wincmd.getWindows = function(){
+    let databuf = ""
+    let scan = spawn("xwininfo", ["-tree","-root" ])
+       scan.stdout.on('data', (data) => { databuf += data });
+       scan.stderr.on('data', (data) => { console.log("stderr",data.toString());});
+       scan.on('exit', (code) => {
+         console.log(`scan exited with code ${code}`);
+
+         let wins = []
+         let lines = databuf.split("\n")
+         lines.forEach((item, i) => {
+             if (i > 5) {
+                 if (!item.includes("(has no name)")){
+                     if (!item.includes("10x10+10+10") && !item.includes("16x16+0+0")){
+                         if (!item.includes("children:")){
+                             if (!item.includes("child:")){
+                                 if (!item.includes("+-")){
+                                     if (item != ""){
+                                         wins.push(item.trim().split('"'))
+                                     }
+                                 }
+                             }
+                         }
+                     }
+                 }
+             }
+         });
+         console.log(wins);
+         let winlist = []
+         wins.forEach((item, i) => {
+             if (item[1] !== item[3] && item[1] !== item[5]){
+                 winlist.push({
+                     id:item[0].trim(),
+                     name:item[1],
+                     geo: item[6].split(" ")[2]
+                 })
+             }
+
+         });
+         wincmd.list = winlist
+         console.log(winlist);
+
+
+         //return databuf
+     })
+}
+
+setTimeout(wincmd.getWindows,2000)
+//console.log(wincmd.getScreenSize());
+//console.log(wincmd.getWindows());
